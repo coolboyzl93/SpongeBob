f4ffbc04123637f82b1fb0497dc5c512e76cd270
zl <1214641362@qq.com>
2017-10-24 17:32:04 +0800

    verson 1.1

--- js/cssControl.js
+++ js/cssControl.js
@@ -6,9 +6,16 @@
   window.onload = function () {
     const IMAGE_CONTAINER = document.querySelector('.detail-image-container')
     const IMAGE_DTEALS = document.querySelector('.detail-image')
+    const THUMBNIAL_ITMES = document.querySelectorAll('.thumbnail-item')
 
     IMAGE_DTEALS.style.maxHeight = IMAGE_CONTAINER.offsetHeight * 0.9 + 'px'
     IMAGE_DTEALS.style.maxWidth = IMAGE_CONTAINER.offsetWidth * 0.9 + 'px'
+
+    let thumbnailArr = [].slice.call(THUMBNIAL_ITMES)
+    thumbnailArr.map( (value) => {
+      value.style.maxWidth = value.children[0].children[0].width + 'px'
+      // console.log(value.children[0].children[0])
+    } )
   // console.log(IMAGE_DTEALS.style.maxHeight + '\n' + IMAGE_CONTAINER.offsetHeight)
   }
 })()
